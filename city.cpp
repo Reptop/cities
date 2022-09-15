57c7ba313e04ae2925bb0ecb1201a9fc708c7da6
Raed Kabir <kabirrraed@raeds-mbp.lan>
2022-09-15 16:37:02 -0700

    WIP

--- city.cpp
+++ city.cpp
@@ -2,7 +2,7 @@
 ** Program: city.cpp
 ** Author: Raed Kabir
 ** Date: 04/08/2017
-** Description: This program 
+** Description: This program compares city and county information 
 ** Input:
 ** Output:
 ******************************************************/
@@ -15,15 +15,18 @@ using namespace std;
 
 county* create_counties(int) {
     county* newCounty = new county();
-
+    
+    cout << "Enter name of county: "; 
     cin >> newCounty->name; 
     cin.clear();
     cin.ignore(1000000, '\n');
-
+    cout << endl;  
+    
     //cin >> newCounty->city; 
     //cin.clear();
     //cin.ignore(1000000, '\n');
-    
+   
+    cout << "Enter number of cities in county: " << endl; 
     cin >> newCounty->cities; 
     cin.clear();
     cin.ignore(1000000, '\n');
@@ -42,21 +45,24 @@ cout << "Enter 0 for state and 1 for county." << endl;
 
   while (run) {
     int choice;
-    vector <county*>* countyList; 
+    vector <county*>* countyList = new vector<county*>();
 
 
-    cout << "Enter Input: ";
+    cout << "Enter Command: ";
     cin >> choice;
     cin.clear();
     cin.ignore(1000000, '\n');
 
     if (choice == 0) {
         cout << '\n';
-       // create_states(int);
+        //create_states(0); // this 0 is a placeholder for now
          
     }
     else if(choice == 1) {
-        //create_states(int); 
+        county* NC = create_counties(0);  
+        countyList->push_back(NC); 
+        cout << "County Entered!" << endl;
+        
     }
     else {
       cout << "Invalid Input. Enter 0 or 1" << endl;
