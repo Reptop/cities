7984bc80c986321060608665a20951a22de73113
Raed Kabir <kabirrraed@raeds-mbp.lan>
2022-09-16 00:12:28 -0700

    fixed gamebreaking bugs

--- city.cpp
+++ city.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <cstring>
+#include <iterator>
 #include <vector>
 #include <fstream>
 #include "fact.h"
@@ -16,23 +17,24 @@ using namespace std;
 
 county * create_counties(int n) {
     county * newCounty = new county();
-    cout << "Enter name of county: "; 
-    cin >> newCounty->name; 
-    cin.clear();
-    cin.ignore(1000000, '\n');
-    cout << endl;
-    newCounty->population = n; 
-    
-    cout << "Enter number of cities in county: " << endl; 
-    cin >> newCounty->cities; 
+    cout << "Enter name of county: ";
+    cin >> newCounty->name;
     cin.clear();
     cin.ignore(1000000, '\n');
 
-    return newCounty; 
+    newCounty->population = n;
+
+    cout << "Enter number of cities in county: " << endl;
+    cin >> newCounty->cities;
+    cin.clear();
+    cin.ignore(1000000, '\n');
+
+    return newCounty;
 }
 
 state * create_states(int) {
   //create state here
+ 
 }
 
 void get_state_data(state *, int, ifstream &) {
@@ -42,11 +44,11 @@ void get_state_data(state *, int, ifstream &) {
 bool is_valid_arguments(char *[], int) {
 
 
-  return true; 
+  return true;
 }
 
 void get_county_data(county *, int, ifstream &) {
-  
+
 }
 
 
@@ -54,64 +56,44 @@ int main() {
 
 bool run = true;
 char choice[20];
+char choice1[20];
 
-cout << "Would you like to create a state or county?" << endl;
-cout << "Enter 0 for state and 1 for county." << endl;
+//storing vector on heap or else segmentation fault. Dont want to use any global vars
+vector <county*> * countyList = new vector<county*>();
 
-  while (run) {
-    cin >> choice;
+cout << "This program compares city and county information" << endl;
+cout << "Command List: Add, Delete, Print" << endl;
 
-    
+while (run) {
+  cout << "Enter Command: ";
+  cin >> choice;
+  cin.clear();
+  cin.ignore(1000000, '\n');
 
-    //storing vector on heap or else segmentation fault. Dont want to use any global vars
-    vector <county*> * countyList = new vector<county*>();
-
-    cout << "Enter Command: ";
-    cout << "Command List: Add, Delete, Print" << endl; 
-    cin >> choice;
+  if (strcmp(choice, "Add") == 0 || strcmp(choice, "add") == 0) {
+    cout << '\n';
+    cout << "Add County or State?" << endl;
+    cin >> choice1;
     cin.clear();
     cin.ignore(1000000, '\n');
 
-    if (strcmp(choice, "Add") == 0 || strcmp(choice, "add") == 0) {
-      cout << '\n';
-      cout << "Add County or State?" << endl; 
-      cout << "Enter command: ";
-      cin >> choice; 
+    if(strcmp(choice, "County") == 0 || strcmp(choice, "county")) {
+      int n;
+      cout << "Enter the population of this county: ";
+      cin >> n;
       cin.clear();
       cin.ignore(1000000, '\n');
-      if(strcmp(choice, "County") == 0 || strcmp(choice, "county")) {
-
-      else if(choice == 1) {
-        int n;
-        cout << "Enter the population of this county: "; 
-        cin >> n; 
-        cin.clear();
-        cin.ignore(1000000, '\n');
-        if(is_valid_arguments(char **, ))
-        county* NC = create_counties(n);  
-        countyList->push_back(NC); 
-        cout << "County Entered!" << endl;
-        
-    }
-    else {
-      cout << "Invalid Input. Enter 0 or 1" << endl;
-    }
+      //if(is_valid_arguments(char **, ))
+      county* NC = create_counties(n);
+      countyList->push_back(NC);
+      cout << "County Entered!" << endl;
       }
     }
-    else if(choice == 1) {
-      int n;
-      cout << "Enter the population of this county: "; 
-      cin >> n; 
-      cin.clear();
-      cin.ignore(1000000, '\n');
-      if(is_valid_arguments(char **, ))
-      county* NC = create_counties(n);  
-      countyList->push_back(NC); 
-      cout << "County Entered!" << endl;
-        
-    }
-    else {
-      cout << "Invalid Input. Enter 0 or 1" << endl;
+    else if (strcmp(choice, "Print") || strcmp(choice, "print")) {
+      for (vector<county*>::iterator iter = countyList->begin(); iter != countyList->end(); ++iter) {
+
+      }
     }
   }
 }
+
